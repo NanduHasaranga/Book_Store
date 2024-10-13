04da8a1de8cd20f49eba012f8eb0029fa5952dcc
Nandu <razergammer201@gmail.com>
2024-10-13 15:41:53 +0530

    Update Single book Description

--- mern-client/src/shop/SingleBook.jsx
+++ mern-client/src/shop/SingleBook.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { useLoaderData } from 'react-router-dom'
 
 const SingleBook = () => {
-    const {_id, bookTitle, imageURL} = useLoaderData();
+    const { _id, bookTitle, imageURL, bookDescription} = useLoaderData();
   return (
-    <div className='mt-28 px-4 lg:px-24 '>
-        <img src={imageURL} alt="" className='h-96' />
-        <h2>{bookTitle}</h2>
+    <div className="mt-28 px-4 lg:px-24 ">
+      <img src={imageURL} alt="" className="h-96" />
+      <h2 style={{ fontWeight: 'bold', fontSize: '1.5em' }}>{bookTitle}</h2>
+      <div>
+        <p>{bookDescription}</p>
+      </div>
     </div>
-  )
+  );
 }
 
 export default SingleBook
